dc25cd68f7985b72c786c0ca111865bf6a7a7ad7
Michael Insalaco <insalaco01@gmail.com>
2018-08-20 14:18:46 -0400

    create methods for 'hit' and 'miss'

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,13 +1,22 @@
 // View Object
 
-var view {
+var view = {
+  // displayMessage method takes an arugment which is a string 
+  // and displays it in the message area
   displayMessage: function(msg) {
-
+    var messageArea = document.getElementById("messageArea"); // get the messageArea element
+    messageArea.innerHTML = msg; // update the messageArea element by setting it to 'msg'
   },
+  
   displayHit: function(location) {
-
+    var cell = document.getElementById(location); // get the id of the passed in string (location)
+    cell.setAttribute("class", "hit"); // sets the class of this element to 'hit'
   },
-  displayMiss: function(location) {
 
+  displayMiss: function(location) {
+    var cell = document.getElementById(location); // get the id of the passed in string (location)
+    cell.setAttribute("class", "miss"); // sets the class of this element to 'hit'
   }
 };
+
+
