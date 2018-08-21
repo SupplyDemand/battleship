8fd7e49f91af10ef1780b7b897eedd1dc1993564
Michael Insalaco <insalaco01@gmail.com>
2018-08-21 14:21:06 -0400

    create Model object and its methods

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,29 +1,6 @@
-// View Object
-
-var view = {
-  // displayMessage method takes an arugment which is a string 
-  // and displays it in the message area
-  displayMessage: function(msg) {
-    var messageArea = document.getElementById("messageArea"); // get the messageArea element
-    messageArea.innerHTML = msg; // update the messageArea element by setting it to 'msg'
-  },
-  
-  displayHit: function(location) {
-    var cell = document.getElementById(location); // get the id of the passed in string (location)
-    cell.setAttribute("class", "hit"); // sets the class of this element to 'hit'
-  },
-
-  displayMiss: function(location) {
-    var cell = document.getElementById(location); // get the id of the passed in string (location)
-    cell.setAttribute("class", "miss"); // sets the class of this element to 'hit'
-  }
-};
-
-
-
 function parseGuess(guess) {
 
-  var alphabet = ["A", "B", "C", "D", "E", "F", "G"]; // create an array of letters
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"]; // create an array of letters and assign it to the variable 'alphabet'
 
   if (guess === null || guess.length !== 2) { // we check for an empty string and if there is a string it is no more than two characters
     alert("please enter a letter and a number");
@@ -39,11 +16,62 @@ function parseGuess(guess) {
       alert("Error - That is off the board! Please try again.");
     
     } else {
-      return row + column // if string 'guess' pass validation then return letter (row) and number (column) and concatenate to form a single sting to two characters
+      return row + column; // if string 'guess' passes validation then return letter (row) and number (column) and concatenate to form a single sting to two characters
     
     }
   }
-}
+};
+
+
+// Model Object
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+           { locations: ["24", "34", "44"], hits: ["", "", ""] },
+           { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+           
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) { // loop though the ships
+      var ship = this.ships[i]; // for each ship create a variable called ship
+      var locations = ship.locations; 
+      var index = locations.indexOf(guess); // if 'guess' is in the locations array then we have a match
+      if (index >= 0) {
+        ship.hits[index] = "hit"; // mark the hits array at the same index
+        return true;
+
+      }
+    }
+    return false; // after looping though all the ships we return false if there are no matches to the 'guess' string
+  }
+};
+
+
+
+// View Object
+var view = {
+  // displayMessage method takes an arugment which is a string 
+  // and displays it in the message area
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea"); // get the messageArea HTML element
+    messageArea.innerHTML = msg; // update the messageArea HTML element by setting it to 'msg'
+  },
+  
+  displayHit: function(location) {
+    var cell = document.getElementById(location); // get the id of the passed in string (location)
+    cell.setAttribute("class", "hit"); // sets the class of this element to 'hit'
+  },
+
+  displayMiss: function(location) {
+    var cell = document.getElementById(location); // get the id of the passed in string (location)
+    cell.setAttribute("class", "miss"); // sets the class of this element to 'hit'
+  }
+};
+
+
+
 
 
 
