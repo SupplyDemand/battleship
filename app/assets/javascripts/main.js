ab30fa3de9bbb9d2b0ca6eb31a31b4d3b6f5a082
Michael Insalaco <insalaco01@gmail.com>
2018-08-21 13:36:10 -0400

    finish parseGuess function

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -25,11 +25,23 @@ function parseGuess(guess) {
 
   var alphabet = ["A", "B", "C", "D", "E", "F", "G"]; // create an array of letters
 
-  if (guess === null || (guess.length !== 2) { // we check for an empty string and if there is a string it is no more than two characters
+  if (guess === null || guess.length !== 2) { // we check for an empty string and if there is a string it is no more than two characters
     alert("please enter a letter and a number");
   } else {
     var firstChar = guess.CharAt(0); // here we grab the first character of the guess string and assign it to 'firstChar'
-    var row = alphabet.indexOf(firstChar) // here grab the index number of the 'firstChar' variable
+    var row = alphabet.indexOf(firstChar); // here we grab the index number of the 'firstChar' variable
+    var column = guess.CharAt(1); // here we grab the second character of the guess string and assing it to 'column'
+    
+    if (isNaN(row) || isNaN(column)) {
+      alert("Error - Enter a letter and number that is on the board. Please try again.");
+
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert("Error - That is off the board! Please try again.");
+    
+    } else {
+      return row + column // if string 'guess' pass validation then return letter (row) and number (column) and concatenate to form a single sting to two characters
+    
+    }
   }
 }
 
