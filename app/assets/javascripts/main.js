753a3c7cf5b50de4318b0a719e2acfe32c28026c
Michael Insalaco <insalaco01@gmail.com>
2018-08-22 16:14:33 -0400

    fix event handlers

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -96,21 +96,43 @@ function parseGuess(guess) {
 	return null;
 }
 
+
+// event handlers
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value.toUpperCase();
+
+	controller.processGuess(guess);
+
+	guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+
+	// in IE9 and earlier, the event object doesn't get passed
+	// to the event handler correctly, so we use window.event instead.
+	e = e || window.event;
+
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+}
+
+
+// init - called when the page has completed loading
+
 window.onload = init;
 
 function init() {
-  var fireButton = document.getElementById("fireButton");
-  fireButton.onclick = handleFireButton;
+	// Fire! button onclick handler
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+
+	// handle "return" key press
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
 
 }
-
-function handleFireButton() {
-  var guessInput = document.getElementById("guessInput");
-  var guess = guessInput.value; // get the user's guess from the input element
-  
-  controller.processGuess(guess); // pass the input guess to the controller
-  
-  guessInput.value = ""; // set the input to empty string
-}
-
-
